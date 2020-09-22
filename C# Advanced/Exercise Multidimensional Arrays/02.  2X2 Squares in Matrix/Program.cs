f5efcc5c7fb40965ed01866ef1d2d77f1cac6b7c
nikolaipetrov2520 <64483062+nikolaipetrov2520@users.noreply.github.com>
2020-09-22 14:34:55 +0300

    New problems solving

---
+++ C# Advanced/Exercise Multidimensional Arrays/02.  2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.__2X2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            char[,] matrix = new char[dimensions[0], dimensions[1]];
+
+            int counter = 0;
+
+            for (int row = 0; row < dimensions[0]; row++)
+            {
+                char[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < dimensions[1]; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            for (int row = 1; row < dimensions[0]; row++)
+            {
+                for (int col = 1; col < dimensions[1]; col++)
+                {
+                    if (matrix[row, col] == matrix[row - 1, col] && matrix[row, col] == matrix[row, col - 1] && matrix[row, col] == matrix[row - 1, col - 1])
+                    {
+                        counter++;
+                    } 
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ C# Advanced/Exercise Multidimensional Arrays/03. Maximal Sum/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+
+namespace _03._Maximal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[dimensions[0], dimensions[1]];
+            int[,] maxSumMatrix = new int[3,3];
+
+            int Maxsum = int.MinValue;
+            int sum = 0;
+
+            for (int row = 0; row < dimensions[0]; row++)
+            {
+                int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int col = 0; col < dimensions[1]; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            for (int row = 2; row < dimensions[0]; row++)
+            {
+                for (int col = 2; col < dimensions[1]; col++)
+                {
+                    sum = matrix[row, col] + matrix[row - 1, col] + matrix[row - 1, col - 1] + matrix[row - 1, col - 2] + matrix[row - 2, col] + matrix[row - 2, col - 1] + matrix[row - 2, col - 2] + matrix[row, col - 1] + matrix[row, col - 2];
+
+                    if (sum > Maxsum)
+                    {
+                        Maxsum = sum;
+                        for (int i = 0; i < 3; i++)
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                maxSumMatrix[i, j] = matrix[row + i - 2, col + j - 2];
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {Maxsum}");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{maxSumMatrix[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
