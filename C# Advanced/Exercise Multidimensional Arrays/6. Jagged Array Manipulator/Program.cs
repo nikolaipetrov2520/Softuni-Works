04cc7ab7291a5b89644f7b3c00b67306746499fe
nikolaipetrov2520 <64483062+nikolaipetrov2520@users.noreply.github.com>
2020-10-14 14:36:40 +0300

    new problems solving

---
+++ C# Advanced/Exercise Multidimensional Arrays/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _6._Jagged_Array_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            double[][] jagged = new double[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] data = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                jagged[row] = new double[data.Length];
+
+                for (int col = 0; col < data.Length; col++)
+                {
+                    jagged[row][col] = data[col];
+                }
+            }
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                if (jagged[row].Length == jagged[row + 1].Length)
+                {
+                    for (int col = 0; col < jagged[row].Length; col++)
+                    {
+                        jagged[row][col] *= 2;
+                        jagged[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < jagged[row].Length; col++)
+                    {
+                        jagged[row][col] /= 2;
+                    }
+                    for (int col = 0; col < jagged[row + 1].Length; col++)
+                    {
+                        jagged[row + 1][col] /= 2;
+                    }
+                }
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] currentInput = input.Split();
+                string command = currentInput[0];
+                int row = int.Parse(currentInput[1]);
+                int col = int.Parse(currentInput[2]);
+                int value = int.Parse(currentInput[3]);
+
+                bool isValid = (row >= 0 && row < jagged.Length) && (col >= 0 && col < jagged[row].Length);
+
+                if (command == "Add")
+                {
+                    if (isValid)
+                    {
+                        jagged[row][col] += value;
+                    }
+                }
+                else if (command == "Subtract")
+                {
+                    if (isValid)
+                    {
+                        jagged[row][col] -= value;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    Console.Write(jagged[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exercise Multidimensional Arrays/7. Knight Game/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace _7._Knight_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] board = new char[n, n];
+            int replaced = 0;
+            int rowKiller = 0;
+            int colKiller = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    board[row, col] = input[col];
+                }
+            }
+
+            while (true)
+            {
+                int maxAttacks = 0;
+
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        int countAttacks = 0;
+
+                        if (board[row, col] == 'K')
+                        {
+                            countAttacks = GetAttacks(board, row, col, countAttacks);
+                        }
+                        if (countAttacks > maxAttacks)
+                        {
+                            maxAttacks = countAttacks;
+                            rowKiller = row;
+                            colKiller = col;
+                        }
+                    }
+                }
+                if (maxAttacks > 0)
+                {
+                    board[rowKiller, colKiller] = '0';
+                    replaced++;
+                }
+                else
+                {
+                    Console.WriteLine(replaced);
+                    break;
+                }
+            }
+        }
+
+        private static int GetAttacks(char[,] board, int row, int col, int countAttacks)
+        {
+            if (IsInside(board, row - 2, col + 1) && board[row - 2, col + 1] == 'K')
+            {
+                countAttacks++;
+            }
+            if (IsInside(board, row - 2, col - 1) && board[row - 2, col - 1] == 'K')
+            {
+                countAttacks++;
+            }
+            if (IsInside(board, row + 1, col + 2) && board[row + 1, col + 2] == 'K')
+            {
+                countAttacks++;
+            }
+            if (IsInside(board, row + 1, col - 2) && board[row + 1, col - 2] == 'K')
+            {
+                countAttacks++;
+            }
+            if (IsInside(board, row - 1, col + 2) && board[row - 1, col + 2] == 'K')
+            {
+                countAttacks++;
+            }
+            if (IsInside(board, row - 1, col - 2) && board[row - 1, col - 2] == 'K')
+            {
+                countAttacks++;
+            }
+            if (IsInside(board, row + 2, col - 1) && board[row + 2, col - 1] == 'K')
+            {
+                countAttacks++;
+            }
+            if (IsInside(board, row + 2, col + 1) && board[row + 2, col + 1] == 'K')
+            {
+                countAttacks++;
+            }
+
+            return countAttacks;
+        }
+
+        private static bool IsInside(char[,] board, int row, int col)
+        {
+            return row >= 0 && row < board.GetLength(0) && col >= 0 && col < board.GetLength(1);
+        }
+    }
+}
