e49913e9fc7149ab0934aa0fb8f36d41f1b16cc8
nikolaipetrov2520 <64483062+nikolaipetrov2520@users.noreply.github.com>
2020-10-15 10:23:08 +0300

    new problem solving

--- C# Advanced/Exercises Sets and Dictionaries Advanced/02.Sets of Elements/Program.cs
+++ C# Advanced/Exercises Sets and Dictionaries Advanced/02.Sets of Elements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.Sets_of_Elements
 {
@@ -6,7 +8,39 @@ namespace _02.Sets_of_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = input[0];
+            int m = input[1];
+
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+            HashSet<int> equalsElement = new HashSet<int>();
+
+
+            for (int i = 0; i < n + m; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (i < n)
+                {
+                    first.Add(num);
+                }
+                else
+                {
+                    second.Add(num);
+                }
+            }
+
+            foreach (var item in first)
+            {
+                if (second.Contains(item))
+                {
+                    equalsElement.Add(item);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", equalsElement));
         }
     }
 }
