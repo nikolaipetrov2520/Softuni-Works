664fccae428e82939e8502d5eeb2acfa2f083760
nikolaipetrov2520 <64483062+nikolaipetrov2520@users.noreply.github.com>
2020-10-16 16:33:54 +0300

    new problems

---
+++ C# Advanced/Exercises Sets and Dictionaries Advanced/04. Even Times/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(num))
+                {
+                    dict.Add(num, 0);
+                }
+                dict[num]++;
+            }
+
+            foreach (var item in dict)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exercises Sets and Dictionaries Advanced/06. Wardrobe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
